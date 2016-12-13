4e07870119c5264ce95a13de9a57c1b739593fef
Faaz Muqeet <faazmuqeet24@gmail.com>
2016-12-13 19:23:08 +0530

    First Commit: Added Entity Data model for Registration

---
+++ OmniMerit-master/OmniMerit/Controllers/DefaultController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+
+namespace OmniMerit.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Career()
+        {
+            if (TempData["Message"]!=null)
+            {
+                ViewBag.Message = "Done Successfully";
+            }
+            return View();
+        }
+        public ActionResult Career1()
+        {
+            ViewBag.Message =null;
+            return View();
+        }
+        public ActionResult Foundation()
+        {
+            ViewBag.Message = null;
+            return View();
+        }
+        public ActionResult Engineering()
+        {
+            
+            return View();
+        }
+        public ActionResult Medical()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ResumeUpload(FormCollection fc, HttpPostedFileBase file)
+        {
+            string name=string.Empty;
+            string email=string.Empty;
+            string path="";
+            if (fc[0].ToString()!=null && fc[1].ToString()!=null)
+            {
+                 name = fc["name"].ToString();
+                 email = fc["email"].ToString();
+            }
+
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                     path = Path.Combine(Server.MapPath("~/Scripts/Images/resume"),
+                                               Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                   
+                    TempData["Message"]= "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+           
+
+
+           
+
+           
+            return RedirectToAction("Career");
+        }
+    }
+}
---
+++ OmniMerit-master/OmniMerit/Controllers/RegController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OmniMerit.Controllers
+{
+    public class RegController : Controller
+    {
+        // GET: Reg
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegUser r)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OmniDbEntities de = new OmniDbEntities())
+                {
+                    de.RegUsers.Add(r);
+                    de.SaveChanges();
+                    ModelState.Clear();
+                    r = null;
+                    ViewBag.Message = "Successfully registration Done. Please lOgin Now";
+                }
+            }
+            return View(r);
+        }
+    }
+}
